d27920cf7526160ad94a3a8d8156fa5a0449cf43
Kushum <kusumtiwari024@gmail.com>
2023-09-01 14:33:07 +0545

    done with the functionalities

--- src/App.js
+++ src/App.js
@@ -7,15 +7,34 @@ function App() {
   const [inputValue , setinputValue] = useState('');
 
   function screenValue(activeBtn){
-   setinputValue(`${inputValue} ${activeBtn}`);
+   setinputValue(`${inputValue}${activeBtn}`);
+  }
+  
+  function calculate(){
+    let result = eval(inputValue);
+    if(!isNaN(result))
+    {
+      prompt("enter a valid expression");
+    }
+    else{
+      setinputValue(result);
+    }
   }
 
+  function clear(){
+    setinputValue('');
+  }
+
+  function removeEach(){
+   let stringvalue = inputValue.toString();
+   setinputValue(stringvalue.slice(0,-1));
+  }
   return (
     <div className="App">
      <h1>Calculator</h1>
      <div className='calc-box'>
       <input className='display-screen' readOnly value={inputValue}></input>
-       <Btn screenvalue={screenValue}/>
+       <Btn screen ={screenValue} displayResult = {calculate} wipeoff = {clear} cancel={removeEach}/>
      </div>
     </div>
   );
--- src/Btn.js
+++ src/Btn.js
@@ -1,14 +1,14 @@
 import {useRef, useState} from 'react';
 
 // const clicked = useRef(null);
-export default function Buttons ({screenvalue}){
+export default function Buttons (props){
 
 const [clicked , setclicked] = useState(false);
 
 const display = (e) => {
  e.target.style.backgroundColor = '#D2B48C';
  setTimeout( () => e.target.style.backgroundColor = 'rgb(218, 204, 204)' , 300);
- screenvalue(e.target.textContent);
+ props.screen(e.target.textContent);
 }
     return(
         <div className='btns'>
@@ -26,9 +26,9 @@ const display = (e) => {
         <button className='sub' onClick={display} >-</button>
         <button className='divide' onClick={display} >/</button>
         <button className='multiply' onClick={display} >*</button>
-        <button className='equalto'>=</button>
-        <button className='delete'>Del</button>
-        <button className='clear'>Clr</button>
+        <button className='equalto' onClick={props.displayResult}>=</button>
+        <button className='delete' onClick={props.cancel}>Del</button>
+        <button className='clear' onClick={props.wipeoff}>Clr</button>
       </div>
     )
 }
